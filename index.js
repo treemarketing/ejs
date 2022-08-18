241cd8e1f39955f4282cd283247f67025e4f8c20
treemarketing <treemarketingonline@gmail.com>
2022-08-18 01:30:26 -0300

    mejoras

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//linea de configuracion 
 app.set('view engine', 'ejs');
 
 let productsHC = [
@@ -20,6 +21,29 @@ let productsHC = [
 ];
 
 app.get('/products', (req, res) => {
-  console.log('aqui products');
-  res.render('pages/index', { title: 'listado de productos', products: productsHC });
+  res.render('pages/products', { title: 'listado de productos', products: productsHC });
+});
+
+
+app.get('/products/:id', (req, res) => {
+  let {id} = req.params;
+  const found = productsHC.find((e) => e.id == id);
+  if (found) {
+    res.render('pages/product', { title: 'UN SOLO PRODUCTO', product: found });
+
+  }else{
+    res.render('pages/error', { msg: 'salio mal' });
+  }
+});
+
+
+app.get('/form', (req, res) => {
+  res.render('pages/form');
+});
+
+app.post('/products', (req, res) => {
+  const {body} = req;
+  //guardar en el array
+  productsHC.push(body)
+  res.redirect(301 ,"/products")
 });
